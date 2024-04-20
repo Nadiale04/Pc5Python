e4f48da1b2815d62eeab6afd1294df13bc9333f2
Nadiale04 <162038699+Nadiale04@users.noreply.github.com>
2024-04-20 19:06:09 +0000

    Soluciones

--- Problema3.py
+++ Problema3.py
@@ -1,5 +1,6 @@
 import pandas as pd
 df_reactiva = pd.read_excel("./data/reactiva.xlsx")
-
-def limpieza(df_reactiva):
-    if 
+#Genere una función de limpieza que permita el renombre de las columnas
+#eliminando espacios, tildes y convirtiendo los nombres de columna a minúscula.
+#De ser necesario cambie el nombre de columna a uno que le sea de más ayuda.
+def f_limpieza(df_reactiva):
